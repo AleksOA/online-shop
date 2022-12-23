defa3de971df9bf436cdc6ab40b5f5e78f231541
AleksOA <ovcharenko.alekss@gmail.com>
2022-12-24 01:58:04 +0200

    source_array

---
+++ js/main.js
@@ -0,0 +1,34 @@
+const prods = [
+    {
+        id: '1p2w',
+        name: 'Product 1',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt',
+        size: ['s', 'l'],
+        price: 29,
+        url: 'https://64.media.tumblr.com/26589a8059a4ea3f4162e16d70719e2c/tumblr_ovfknb7wtR1slhhf0o1_1280.jpg'
+    },
+    {
+        id: '2g4qq',
+        name: 'Product 2',
+        description: 'Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat',
+        size: ['s', 'm', 'l'],
+        price: 35,
+        url: 'https://64.media.tumblr.com/7918ca72d8cb3566a37fbd627dcb24ac/tumblr_oupo142eEs1slhhf0o1_1280.jpg'
+    },
+    {
+        id: '43gl88',
+        name: 'Product 3',
+        description: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        size: [],
+        price: 35,
+        url: 'https://64.media.tumblr.com/4b5e14b48ae5129f5fd9d6eb8406cb50/tumblr_otdi8010Sf1slhhf0o1_1280.jpg'
+    },
+    {
+        id: '87cxL3',
+        name: 'Product 4',
+        description: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore',
+        size: ['m', 'xl'],
+        price: 35,
+        url: 'https://64.media.tumblr.com/7bed8273f621486faecdd407969aef51/tumblr_otfcxeacdO1slhhf0o1_1280.jpg'
+    },
+]
